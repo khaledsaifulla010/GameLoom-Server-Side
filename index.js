7f20b16242cbc8afd5207b626e337d7a23fdc037
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-04 22:20:11 +0600

    get high rated games datas from mongodb

--- index.js
+++ index.js
@@ -11,12 +11,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 // CONNECT WITH MONGOBD//
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ugbxhsw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -27,22 +25,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // GET HIGHEST RATED GAMES //
+
+    const GameLoomCollection = client
+      .db("GameLoom")
+      .collection("HighRatedGames");
+
+    app.get("/highRatedGames", async (req, res) => {
+      const cursor = GameLoomCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
 
 // SERVER RUNNING //
 
